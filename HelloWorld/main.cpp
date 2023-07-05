b5adbe8541ca16d51e87e46ecd2b3c0a07f311c5
Anderson Ito <andersonkmi@acm.org>
2023-07-05 04:25:47 +0200

    changes

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -8,10 +8,21 @@
 
 #include <iostream>
 #include <cstdlib>
+#include "Stash.hpp"
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
-	std::cout << "Hello world again" << std::endl;
+	// Creating an int stash object
+	Stash* intStash = new Stash(sizeof(int));
+	for (int i = 0; i < 100; i++) {
+		intStash->add(&i);
+	}
+
+	for (int index = 0; index < intStash->count(); index++) {
+		std::cout << *(int*) intStash->fetch(index) << std::endl;
+	}
+
+	delete(intStash);
 	return EXIT_SUCCESS;
 }
